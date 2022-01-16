2c16023c822c37af69904cd5432e2c4a28f77b23
wa20 <78425538+wa20@users.noreply.github.com>
2022-01-16 13:09:27 +0000

    fortmating

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,33 @@
 // stored note data
-const noteData = require("../db/data");
 const router = require("express").Router();
+const noteData = require("../db/data");
 
-//get requests for notes page
-
+//GETrequests for notes page
 router.get("/api/notes", (req, res) => {
-  console.log("get request called")
+  console.log("get request called");
+
   noteData.getAllNotes().then((notes) => {
-      return res.json(notes)
-  })
+    return res.json(notes);
+  });
 });
 
+//POST Request
 router.post("/api/notes", (req, res) => {
-    console.log("post request called")
-    noteData.postNote(req.body).then((note) => {
-        res.json(note)})
+  console.log("post request called");
+  noteData
+    .postNote(req.body)
+    .then((note) => res.json(note))
+    .catch((err) => res.status(500).json(err));
 });
 
+//Delete Request
 router.delete("/api/notes/:id", (req, res) => {
-    console.log("delete request called")
-    const id = req.params.id; 
-    noteData.deleteNote(id).then(()=> 
-    res.json({Response: "Successful"})).catch((error) => res.json(error))
+  console.log("delete request called");
+  const id = req.params.id;
+  noteData
+    .deleteNote(req.params.id)
+    .then(() => res.json({ ok: true, Response: "Successful" }))
+    .catch((error) => res.json(error));
 });
 
 module.exports = router;
